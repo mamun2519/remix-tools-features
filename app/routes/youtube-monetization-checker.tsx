5584ee3792c40f5512b365d12c357739409f2154
mamun2519 <juborajislam46@gmail.com>
2024-12-12 10:27:40 +0600

    complete youtube monetization cheker tools implement successfully

--- app/routes/youtube-monetization-checker.tsx
+++ app/routes/youtube-monetization-checker.tsx
@@ -155,7 +155,21 @@ async function checkMonetization(channelId: string): Promise<boolean> {
 const YoutubeMonetizationChecker = () => {
   const actionData = useActionData<typeof action>();
   return (
-   
+    <div className="p-10">
+      <div className="mt-10 h-96 rounded border p-6">
+        <h3>YouTube Monetization Checker</h3>
+
+        <div className="mt-2">
+          <Form method="post">
+            <input
+              type="text"
+              name="youtube-url"
+              placeholder="Enter Youtube channel url"
+              className="h-14 w-96 rounded border px-4 outline-slate-300"
+            />
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
