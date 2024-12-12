71109adc5bdbeda4a38289e28bbde75cbcdd40b8
mamun2519 <juborajislam46@gmail.com>
2024-12-12 10:25:31 +0600

    complete youtube monetization cheker tools implement successfully

--- app/routes/youtube-monetization-checker.tsx
+++ app/routes/youtube-monetization-checker.tsx
@@ -3,13 +3,3 @@ import { Form, useActionData } from "@remix-run/react";
 import { google } from "googleapis";
 
 const YOUTUBE_API_KEY = "AIzaSyAzXwAtQP499bdtT6zcPL-vQd72elaJs7Y";
-async function findChannelIdByHandle(
-  youtube: any,
-  handle: string,
-): Promise<string | null> {
-  try {
-  } catch (error) {
-    console.error("Error finding channel ID:", error);
-    return null;
-  }
-}
