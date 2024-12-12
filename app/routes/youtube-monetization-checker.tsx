49a0e430c6a85c95cc2e9a0306d1b10c6c2bf5b9
mamun2519 <juborajislam46@gmail.com>
2024-12-12 10:25:39 +0600

    complete youtube monetization cheker tools implement successfully

--- app/routes/youtube-monetization-checker.tsx
+++ app/routes/youtube-monetization-checker.tsx
@@ -1,5 +1,3 @@
 import { ActionFunctionArgs, json } from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
 import { google } from "googleapis";
-
-const YOUTUBE_API_KEY = "AIzaSyAzXwAtQP499bdtT6zcPL-vQd72elaJs7Y";
