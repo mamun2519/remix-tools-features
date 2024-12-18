2958b0c3cdc6e1acb4bccd0ff36ee2d0aa862f2f
mamun2519 <juborajislam46@gmail.com>
2024-12-18 08:41:54 +0600

    added to youtube tag generator tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,11 @@ import type { MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
+  const [items, setItems] = useState([
+    { id: 1, name: "Item 1" },
+    { id: 2, name: "Item 2" },
+    { id: 3, name: "Item 3" },
+  ]);
   return (
     <main className="p-10">
       <div className="h-96 w-full rounded-lg border p-5 shadow-sm">
