e4c9d3b188a480a5cf46909e245f2abedb15408a
mamun2519 <juborajislam46@gmail.com>
2024-12-18 08:39:03 +0600

    added to youtube tag generator tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   return (
     <main className="p-10">
       <div className="h-96 w-full rounded-lg border p-5 shadow-sm">
-        <h1 className="text-3xl font-semibold">Bare Metal Remix</h1>
+        <h1 className="font-semibold">Our free tools list</h1>
       </div>
     </main>
   );
