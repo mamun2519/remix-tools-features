b060b4d38dd8bfcb60ade1c58f963c99dffe9247
mamun2519 <juborajislam46@gmail.com>
2024-12-18 08:38:00 +0600

    added to youtube tag generator tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 export default function Index() {
   return (
     <main className="p-10">
-      <div className="h-96 w-full rounded-lg border"></div>
+      <div className="h-96 w-full rounded-lg border shadow-sm"></div>
       <h1 className="text-3xl font-semibold">Bare Metal Remix</h1>
     </main>
   );
