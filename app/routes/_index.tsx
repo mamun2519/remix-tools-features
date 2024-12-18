6ce82459094d3895117f5fa136e157df89d21a61
mamun2519 <juborajislam46@gmail.com>
2024-12-18 08:43:43 +0600

    added to youtube tag generator tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,23 +13,25 @@ export default function Index() {
     <main className="p-10">
       <div className="h-96 w-full rounded-lg border p-5 shadow-sm">
         <h1 className="text-xl font-semibold">Our free tools list</h1>
-        <h1 className="mb-4 text-2xl font-bold">Item List</h1>
-        <ul className="space-y-4">
-          {items.map((item) => (
-            <li
-              key={item.id}
-              className="flex items-center justify-between rounded-md bg-white p-4 shadow-md hover:bg-gray-50"
-            >
-              <span className="text-lg font-medium">{item.name}</span>
-              <button
-                onClick={() => alert(`You clicked on ${item.name}`)}
-                className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+        {/* <h1 className="mb-4 text-2xl font-bold">Item List</h1> */}
+        <div className="grid grid-cols-2">
+          <ul className="space-y-4">
+            {items.map((item) => (
+              <li
+                key={item.id}
+                className="flex items-center justify-between rounded-md bg-white p-4 shadow-md hover:bg-gray-50"
               >
-                View
-              </button>
-            </li>
-          ))}
-        </ul>
+                <span className="text-lg font-medium">{item.name}</span>
+                <button
+                  onClick={() => alert(`You clicked on ${item.name}`)}
+                  className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                >
+                  View
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
