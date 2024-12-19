357630e269b016d9234ac90ac016006266e463b4
mamun2519 <juborajislam46@gmail.com>
2024-12-19 08:39:24 +0600

    feat: added to youtube vedio description tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -54,7 +54,9 @@ export default function Index() {
   return (
     <main className="p-10">
       <div className="h-full w-full rounded-lg border p-5 shadow-sm">
-        <h1 className="mb-4 text-2xl font-bold">Our free tools list</h1>
+        <h1 className="mb-4 text-2xl font-bold uppercase">
+          Our free tools list
+        </h1>
         {/* <h1 className="mb-4 text-2xl font-bold">Item List</h1> */}
         <div className="grid grid-cols-3">
           <ul className="space-y-4">
@@ -62,7 +64,7 @@ export default function Index() {
             {items.map((item) => (
               <li
                 key={item.id}
-                className="flex items-center justify-between rounded-md border bg-white p-4 hover:bg-gray-50"
+                className="flex items-center justify-between rounded-md border bg-white p-2 hover:bg-gray-50"
               >
                 <span className="font-medium">{item.name}</span>
                 <div className="flex gap-5">
