1ed5cffa746d4a27e86b8e2f8564f6d967f25e58
mamun2519 <juborajislam46@gmail.com>
2024-12-22 09:13:05 +0600

    feat: added to transcript downloader tools

--- app/routes/youtube-transcript-generator.tsx
+++ app/routes/youtube-transcript-generator.tsx
@@ -79,6 +79,7 @@ Time: ${new Date().toISOString()}
   return stl;
 };
 
+//* convert content to Sub format
 const convertToSUB = (transcript: string[]) => {
   return transcript
     .map((text, index) => {
