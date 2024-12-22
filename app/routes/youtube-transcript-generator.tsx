cbfc0dbc16e7d6e0b357e3aa32d656dd7fd026d0
mamun2519 <juborajislam46@gmail.com>
2024-12-22 07:16:16 +0600

    feat: added to transcript downloader tools

--- app/routes/youtube-transcript-generator.tsx
+++ app/routes/youtube-transcript-generator.tsx
@@ -22,7 +22,7 @@ export async function action({ request }: ActionFunctionArgs) {
     // Get transcript
     const transcriptItems = await YoutubeTranscript.fetchTranscript(videoId);
 
-    // Format transcript with timestampss
+    // Format transcript with timestamps
     const formattedTranscript = transcriptItems
       .map((item) => {
         const decodedText = decodeHTMLEntities(item.text);
