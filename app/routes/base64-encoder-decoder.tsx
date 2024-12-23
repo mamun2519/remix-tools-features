504cde20d6c9ce79bf6bdd37058068ccfbb4b41f
mamun2519 <juborajislam46@gmail.com>
2024-12-23 09:27:15 +0600

    feat: added to base64 encoder & decoder tools

--- app/routes/base64-encoder-decoder.tsx
+++ app/routes/base64-encoder-decoder.tsx
@@ -1,7 +1,9 @@
+import { useActionData } from "@remix-run/react";
 import iconv from "iconv-lite";
 import { useState } from "react";
 
 const Base64EncodedDecodedConverter = () => {
+  const actionData = useActionData();
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
   return <div>Hello world</div>;
