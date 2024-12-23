2dfb7f4c4db9fc8c0b5ec620640a691b403556a9
mamun2519 <juborajislam46@gmail.com>
2024-12-23 09:25:46 +0600

    feat: added to base64 encoder & decoder tools

--- app/routes/base64-encoder-decoder.tsx
+++ app/routes/base64-encoder-decoder.tsx
@@ -1,13 +1,4 @@
 import iconv from "iconv-lite";
-export function encodeToBase64(input: string, charset = "utf-8") {
-  const buffer = iconv.encode(input, charset);
-  return buffer.toString("base64");
-}
-
-export function decodeFromBase64(input: string, charset = "utf-8") {
-  const buffer = Buffer.from(input, "base64");
-  return iconv.decode(buffer, charset);
-}
 
 const Base64EncodedDecodedConverter = () => {
   const [input, setInput] = useState("");
