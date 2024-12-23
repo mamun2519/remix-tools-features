a3f0254dd9e44ff513bb74d16c2d57550b5c0210
mamun2519 <juborajislam46@gmail.com>
2024-12-23 06:44:36 +0600

    feat: added to youtube-summrize tools

---
+++ app/routes/youtube-summrize.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const youtube-summrize = () => {
+      return (
+            <div>
+                  
+            </div>
+      );
+};
+
+export default youtube-summrize;
