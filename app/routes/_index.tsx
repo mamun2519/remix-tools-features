c2d4e867256de83419d37af4c8f4cef6d9570461
mamun2519 <juborajislam46@gmail.com>
2024-12-23 06:53:05 +0600

    feat: added to youtube-summrize tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,64 +6,7 @@ export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
   const navigate = useNavigate();
-  const [youtubeItems, setYoutubeItems] = useState([
-    { id: 1, name: "Youtube Money Calculator", link: "", docs: "" },
-    {
-      id: 2,
-      name: "Youtube Monetization Checker",
-      link: "youtube-monetization-checker",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Monetization-Checker-15f380f1a04580a9b3d1c6052fc02d27?pvs=4",
-    },
-    {
-      id: 3,
-      name: "Youtube Name Generator",
-      link: "youtube-name-generate",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/YouTube-Name-Generator-15f380f1a04580aea0c3e9c158cbee72?pvs=4",
-    },
-    {
-      id: 4,
-      name: "Youtube Title Generator",
-      link: "youtube-title-generator",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Title-Generator-15f380f1a0458090a968e07d896d1f60?pvs=4",
-    },
-    {
-      id: 5,
-      name: "Youtube Tags Generator",
-      link: "youtube-tag-generator",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Tag-Generator-160380f1a0458048a419eacc1c34b054?pvs=4",
-    },
-    {
-      id: 6,
-      name: "YouTube Thumbnail Downloader",
-      link: "youTube-thumbnail-downloader",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Thumbnail-Downloader-161380f1a04580498119ebdc897374b5?pvs=4",
-    },
-    {
-      id: 7,
-      name: "YouTube Tag Extractor",
-      link: "youTube-tag-extractor",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-tag-Extractor-161380f1a04580ca8e17dff94c9f9b3a?pvs=4",
-    },
-
-    {
-      id: 8,
-      name: "YouTube Description Generator",
-      link: "youTube-description-generator",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/YouTube-Description-Generator-161380f1a045805db960f33ab9aec72a?pvs=4",
-    },
-    {
-      id: 9,
-      name: "YouTube Transcript Generator",
-      link: "youtube-transcript-generator",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Transcript-Generator-163380f1a04580fc8a48da7d50b14b86?pvs=4",
-    },
-    {
-      id: 10,
-      name: "YouTube Summarize",
-      link: "youtube-summarize",
-      docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Summarize-165380f1a04580af98a7e50177c379ac?pvs=4",
-    },
-  ]);
+  const [youtubeItems, setYoutubeItems] = useState();
   const [tools, setTools] = useState([]);
 
   return (
@@ -76,7 +19,7 @@ export default function Index() {
         <div className="grid grid-cols-3">
           <ul className="space-y-4">
             <h1 className="mb-4 text-2xl font-bold">Youtube Tools</h1>
-            {items.map((item) => (
+            {youtubeItems.map((item) => (
               <li
                 key={item.id}
                 className="flex items-center justify-between rounded-md border bg-white p-2 hover:bg-gray-50"
---
+++ app/utils.ts
@@ -0,0 +1,58 @@
+const youtubeToolsList = [
+  { id: 1, name: "Youtube Money Calculator", link: "", docs: "" },
+  {
+    id: 2,
+    name: "Youtube Monetization Checker",
+    link: "youtube-monetization-checker",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Monetization-Checker-15f380f1a04580a9b3d1c6052fc02d27?pvs=4",
+  },
+  {
+    id: 3,
+    name: "Youtube Name Generator",
+    link: "youtube-name-generate",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/YouTube-Name-Generator-15f380f1a04580aea0c3e9c158cbee72?pvs=4",
+  },
+  {
+    id: 4,
+    name: "Youtube Title Generator",
+    link: "youtube-title-generator",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Title-Generator-15f380f1a0458090a968e07d896d1f60?pvs=4",
+  },
+  {
+    id: 5,
+    name: "Youtube Tags Generator",
+    link: "youtube-tag-generator",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Tag-Generator-160380f1a0458048a419eacc1c34b054?pvs=4",
+  },
+  {
+    id: 6,
+    name: "YouTube Thumbnail Downloader",
+    link: "youTube-thumbnail-downloader",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Thumbnail-Downloader-161380f1a04580498119ebdc897374b5?pvs=4",
+  },
+  {
+    id: 7,
+    name: "YouTube Tag Extractor",
+    link: "youTube-tag-extractor",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-tag-Extractor-161380f1a04580ca8e17dff94c9f9b3a?pvs=4",
+  },
+
+  {
+    id: 8,
+    name: "YouTube Description Generator",
+    link: "youTube-description-generator",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/YouTube-Description-Generator-161380f1a045805db960f33ab9aec72a?pvs=4",
+  },
+  {
+    id: 9,
+    name: "YouTube Transcript Generator",
+    link: "youtube-transcript-generator",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Transcript-Generator-163380f1a04580fc8a48da7d50b14b86?pvs=4",
+  },
+  {
+    id: 10,
+    name: "YouTube Summarize",
+    link: "youtube-summarize",
+    docs: "https://kiwi-parrotfish-5f9.notion.site/Youtube-Summarize-165380f1a04580af98a7e50177c379ac?pvs=4",
+  },
+];
