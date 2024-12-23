682b0d04217ed96de421bca6af5ec469096719a6
mamun2519 <juborajislam46@gmail.com>
2024-12-23 07:26:37 +0600

    feat: added to youtube-summrize tools

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,14 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import { useState } from "react";
-import { youtubeToolsList } from "~/utils";
+import { otherTools, youtubeToolsList } from "~/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
   const navigate = useNavigate();
   const [youtubeItems, setYoutubeItems] = useState(youtubeToolsList || []);
-  const [tools, setTools] = useState([]);
+  const [tools, setTools] = useState(otherTools || otherTools);
 
   return (
     <main className="p-10">
@@ -17,10 +17,38 @@ export default function Index() {
           Our free tools list
         </h1>
         {/* <h1 className="mb-4 text-2xl font-bold">Item List</h1> */}
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 gap-5">
           <ul className="space-y-4">
             <h1 className="mb-4 text-2xl font-bold">Youtube Tools</h1>
             {youtubeItems.map((item) => (
+              <li
+                key={item.id}
+                className="flex items-center justify-between rounded-md border bg-white p-2 hover:bg-gray-50"
+              >
+                <span className="font-medium">{item.name}</span>
+                <div className="flex gap-5">
+                  <a
+                    target="_blank"
+                    className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+                    href={item.docs}
+                    rel="noreferrer"
+                  >
+                    Docs
+                  </a>
+                  <button
+                    onClick={() => navigate(item.link)}
+                    className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+                  >
+                    View
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <ul className="space-y-4">
+            <h1 className="mb-4 text-2xl font-bold">Other Tools</h1>
+            {tools.map((item) => (
               <li
                 key={item.id}
                 className="flex items-center justify-between rounded-md border bg-white p-2 hover:bg-gray-50"
