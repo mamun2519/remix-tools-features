408e79d7e834319b0d3fd7f2df4361afbf41dc8e
mamun2519 <juborajislam46@gmail.com>
2024-12-23 06:56:26 +0600

    feat: added to youtube-summrize tools

--- app/routes/youtube-summarize.tsx
+++ app/routes/youtube-summarize.tsx
@@ -1,3 +1,46 @@
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const videoUrl = formData.get("videoURL") as string;
+
+  try {
+    // Extract video ID from URL
+    const videoId = extractVideoId(videoUrl);
+    if (!videoId) {
+      throw new Error("Invalid YouTube URL");
+    }
+
+    // Initialize Youtube
+    const youtube = await Innertube.create({
+      lang: "en",
+      location: "US",
+      retrieve_player: false,
+    });
+
+    //* get video info by video id
+    const info = await youtube.getInfo(videoId);
+
+    //* get video transcript
+    const transcriptData = await info.getTranscript();
+    // transcript data
+    const transcript =
+      transcriptData?.transcript?.content?.body?.initial_segments?.map(
+        (segment) => segment.snippet.text,
+      );
+
+    return json({
+      success: true,
+      transcript: transcript,
+      videoId: videoId,
+    });
+  } catch (error) {
+    return json({
+      success: false,
+      error:
+        error instanceof Error ? error.message : "Failed to get transcript",
+    });
+  }
+}
+
 const YoutubeSummarize = () => {
   return <div>This is youtube summarize tools</div>;
 };
