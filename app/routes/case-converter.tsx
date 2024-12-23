2b00179106ba83b464d1c675a515ad3fd3204300
mamun2519 <juborajislam46@gmail.com>
2024-12-23 07:35:01 +0600

    feat: added to youtube-summrize tools

--- app/routes/case-converter.tsx
+++ app/routes/case-converter.tsx
@@ -1,10 +1,52 @@
-import React from "react";
-
 const CaseConverter = () => {
   const actionData = useActionData();
   const navigation = useNavigation();
   const isGenerating = navigation.state === "submitting";
-  return <div>This is case converter</div>;
+  return (
+    <div className="p-10">
+      <div className="mt-10 h-full rounded border p-6">
+        <h3>Text Converter</h3>
+
+        <div className="mt-2">
+          <Form method="post">
+            <input
+              type="text"
+              name="videoURL"
+              placeholder="Enter Youtube Video URL"
+              className="h-14 w-96 rounded border px-4 outline-slate-300"
+            />
+
+            <button
+              type="submit"
+              className="h-14 border bg-red-400 px-4 font-bold text-white"
+            >
+              {isGenerating ? "Summarizing..." : "Summarize"}
+            </button>
+          </Form>
+        </div>
+
+        {actionData?.error && (
+          <div className="mt-4 text-center text-red-600">
+            {actionData.error}
+          </div>
+        )}
+
+        {actionData?.transcript && (
+          <>
+            <div className="text-md mt-10 max-h-[400px] overflow-y-auto rounded-md border p-4 text-justify">
+              {actionData?.transcript?.map((line, index) => {
+                return (
+                  <div key={index} className={index !== 0 && "mt-4"}>
+                    <span className="text-gray-90">{line}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CaseConverter;
