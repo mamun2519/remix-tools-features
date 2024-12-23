4b4f8f41899631444e3b9607df2473e68dcfa7a7
mamun2519 <juborajislam46@gmail.com>
2024-12-23 07:34:23 +0600

    feat: added to youtube-summrize tools

--- app/routes/case-converter.tsx
+++ app/routes/case-converter.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const CaseConverter = () => {
+  const actionData = useActionData();
+  const navigation = useNavigation();
+  const isGenerating = navigation.state === "submitting";
   return <div>This is case converter</div>;
 };
 
