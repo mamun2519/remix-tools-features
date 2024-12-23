e0a9175c0d8b6e16e95461f7d25896efcd7aa726
mamun2519 <juborajislam46@gmail.com>
2024-12-23 07:00:32 +0600

    feat: added to youtube-summrize tools

--- app/routes/youtube-summarize.tsx
+++ app/routes/youtube-summarize.tsx
@@ -62,7 +62,65 @@ export const meta: MetaFunction = () => {
 };
 
 const YoutubeSummarize = () => {
-  return <div>This is youtube summarize tools</div>;
+  return (
+    <div className="p-10">
+      <div className="mt-10 h-full rounded border p-6">
+        <h3>YouTube Transcript Generate</h3>
+
+        <div className="mt-2">
+          <Form method="post">
+            <input
+              type="text"
+              name="videoURL"
+              placeholder="Enter Youtube Video URL"
+              className="h-14 w-96 rounded border px-4 outline-slate-300"
+            />
+
+            <button
+              type="submit"
+              className="h-14 border bg-red-400 px-4 font-bold text-white"
+            >
+              {isGenerating ? "Generating..." : "Generate Transcript"}
+            </button>
+          </Form>
+        </div>
+
+        {actionData?.error && (
+          <div className="mt-4 text-center text-red-600">
+            {actionData.error}
+          </div>
+        )}
+
+        {actionData?.transcript && (
+          <>
+            <div className="text-md mt-10 max-h-[400px] overflow-y-auto rounded-md border p-4 text-justify">
+              {actionData?.transcript?.map((line, index) => {
+                return (
+                  <div key={index} className={index !== 0 && "mt-4"}>
+                    <span className="text-gray-90">{line}</span>
+                  </div>
+                );
+              })}
+            </div>
+            {/* Download transcript button */}
+            <div className="mt-4 flex flex-wrap gap-2">
+              {["SRT", "VTT", "TTML", "STL", "SUB", "DFXP", "SBV", "TXT"].map(
+                (format) => (
+                  <button
+                    key={format}
+                    onClick={() => handleDownload(format)}
+                    className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+                  >
+                    Download {format}
+                  </button>
+                ),
+              )}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default YoutubeSummarize;
