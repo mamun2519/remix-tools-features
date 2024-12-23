68771f4d006abf27a362b81dfb82a2178fcd8d1d
mamun2519 <juborajislam46@gmail.com>
2024-12-23 09:22:01 +0600

    feat: added to base64 encoder & decoder tools

--- app/routes/base64-encoder-decoder.tsx
+++ app/routes/base64-encoder-decoder.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+export function encodeToBase64(input, charset = "utf-8") {
+  const buffer = iconv.encode(input, charset);
+  return buffer.toString("base64");
+}
+
+export function decodeFromBase64(input, charset = "utf-8") {
+  const buffer = Buffer.from(input, "base64");
+  return iconv.decode(buffer, charset);
+}
 
 const Base64EncodedDecodedConverter = () => {
   return <div>Hello world</div>;
