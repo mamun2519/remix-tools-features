5a59a3fe198a350ae8cde2c7f4fbccf5e232e30a
mamun2519 <juborajislam46@gmail.com>
2024-12-23 09:28:14 +0600

    feat: added to base64 encoder & decoder tools

--- app/routes/base64-encoder-decoder.tsx
+++ app/routes/base64-encoder-decoder.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 
 const Base64EncodedDecodedConverter = () => {
   const actionData = useActionData();
-  const [input, setInput] = useState("");
-  const [output, setOutput] = useState("");
+  //   const [input, setInput] = useState("");
+  //   const [output, setOutput] = useState("");
   return (
     <div className="mx-auto max-w-4xl p-6">
       <h1 className="mb-4 text-2xl font-bold">Base64 Encoder/Decoder</h1>
