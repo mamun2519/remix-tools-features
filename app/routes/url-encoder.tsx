40e4f65aae39bab236fffadc12446076159a6e9e
mamun2519 <juborajislam46@gmail.com>
2024-12-24 07:16:13 +0600

    feat: added to youtube url encoded tools

--- app/routes/url-encoder.tsx
+++ app/routes/url-encoder.tsx
@@ -1,4 +1,5 @@
 import { MetaFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,6 +15,7 @@ const UrlEncodedTools = () => {
   return (
     <div className="mx-auto mt-20 max-w-4xl border p-6">
       <h1 className="mb-4 text-2xl font-bold">Base64 Url encoded Tools</h1>
+      <Form action="post" className="space-y-4"></Form>
     </div>
   );
 };
