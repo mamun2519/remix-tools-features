1b7fa32c0214fbf8fdae9da84eb282722f0484de
mamun2519 <juborajislam46@gmail.com>
2024-12-24 07:16:47 +0600

    feat: added to youtube url encoded tools

--- app/routes/url-encoder.tsx
+++ app/routes/url-encoder.tsx
@@ -15,7 +15,11 @@ const UrlEncodedTools = () => {
   return (
     <div className="mx-auto mt-20 max-w-4xl border p-6">
       <h1 className="mb-4 text-2xl font-bold">Base64 Url encoded Tools</h1>
-      <Form action="post" className="space-y-4"></Form>
+      <Form action="post" className="space-y-4">
+        <div>
+          <label htmlFor=""></label>
+        </div>
+      </Form>
     </div>
   );
 };
