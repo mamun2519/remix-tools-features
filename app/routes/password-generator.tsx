954f8b0dd4f90a17829b6f6bcbe426c2d4d6c5e4
mamun2519 <juborajislam46@gmail.com>
2024-12-24 10:13:07 +0600

    feat: added to password generator  tools

--- app/routes/password-generator.tsx
+++ app/routes/password-generator.tsx
@@ -31,7 +31,6 @@ export default function PasswordGenerator() {
   });
   const [passwords, setPasswords] = useState<string[]>([]);
 
-  // Generate passwords whenever length or options change
   useEffect(() => {
     const generated = generatePassword({ length, ...options });
     setPasswords(generated);
