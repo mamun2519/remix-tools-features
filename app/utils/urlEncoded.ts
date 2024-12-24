357f0e287f8bcbef8974e45aecbf0478a6d0d59f
mamun2519 <juborajislam46@gmail.com>
2024-12-24 08:32:35 +0600

    feat: added to youtube url encoded tools

--- app/utils/urlEncoded.ts
+++ app/utils/urlEncoded.ts
@@ -1,12 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const urlEncoded = (text: any, charset = "utf-8" as any) => {
   const buffer = Buffer.from(text, charset);
-  const result = buffer.toString("base64");
-  return result;
+  const encoded = buffer.toString("base64");
+  const urlSafeBase64 = encoded
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, ""); // URL-safe encoding
+  return urlSafeBase64;
 };
 
 export const urlDecoded = (text: any, charset = "utf-8" as any) => {
-  const buffer = Buffer.from(text, "base64");
-  const result = buffer.toString(charset);
-  return result;
+  const base64 = text.replace(/-/g, "+").replace(/_/g, "/"); // Convert URL-safe Base64 to standard Base64
+  const buffer = Buffer.from(base64, "base64");
+  const decoded = buffer.toString(charset);
+  return decoded;
 };
