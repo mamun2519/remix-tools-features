547424bbd52790a9b51597f1e01111780e17c12f
mamun2519 <juborajislam46@gmail.com>
2024-12-24 08:41:23 +0600

    feat: added to youtube url encoded tools

--- app/routes/url-encoder.tsx
+++ app/routes/url-encoder.tsx
@@ -10,10 +10,6 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const charset = formData.get("charset") || "utf-8";
   const operation = formData.get("operation") || "encode";
 
-  console.log("inputText", inputText);
-  console.log("charset", charset);
-  console.log("operation", operation);
-
   try {
     let result;
     if (operation == "encode") {
