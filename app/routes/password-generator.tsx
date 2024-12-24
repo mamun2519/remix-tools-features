85b3b052121578025feee01975d6b387a0b6c72f
mamun2519 <juborajislam46@gmail.com>
2024-12-24 09:39:12 +0600

    feat: added to password generator  tools

--- app/routes/password-generator.tsx
+++ app/routes/password-generator.tsx
@@ -1,3 +1,14 @@
+import { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Url encoded" },
+    {
+      name: "description",
+      content: "url encoded tools",
+    },
+  ];
+};
 const PasswordGeneratorTools = () => {
   return <div>Hello password generator tools</div>;
 };
