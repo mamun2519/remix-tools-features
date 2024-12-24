4b405792b64fb0d8a54d42392c1fdf60e163af62
mamun2519 <juborajislam46@gmail.com>
2024-12-24 08:03:47 +0600

    feat: added to youtube url encoded tools

--- app/utils/urlEncoded.ts
+++ app/utils/urlEncoded.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const urlEncoded = (text: any, charset = "utf-8" as any) => {
   const buffer = Buffer.from(text, charset);
+  const result = buffer.toString("base64");
 };
