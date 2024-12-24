13b2ebee1dfb677cfd61c19b21379c21b1e95833
mamun2519 <juborajislam46@gmail.com>
2024-12-24 07:41:55 +0600

    feat: added to youtube url encoded tools

--- app/routes/url-encoder.tsx
+++ app/routes/url-encoder.tsx
@@ -74,7 +74,14 @@ const UrlEncodedTools = () => {
         {actionData && (
           <div className="mt-6">
             <h2 className="mb-2 text-xl font-bold">Output</h2>
-            {actionData.success ? <textarea name="" id=""></textarea> : null}
+            {actionData.success ? (
+              <textarea
+                value={actionData.output}
+                readOnly
+                rows="4"
+                className="w-full rounded border bg-gray-100 p-2"
+              ></textarea>
+            ) : null}
           </div>
         )}
       </Form>
