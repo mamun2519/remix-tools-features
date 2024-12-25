35f6d4a1b11c6223b96d10143e2f0fc852e46842
mamun2519 <juborajislam46@gmail.com>
2024-12-25 08:46:08 +0600

    feat: added to youtube summarize tools

--- app/routes/youtube-summarize.tsx
+++ app/routes/youtube-summarize.tsx
@@ -60,11 +60,9 @@ export async function action({ request }: ActionFunctionArgs) {
     const content = response.choices[0].message?.content;
     console.log("content", content);
 
-    const [summary, outlines, mindmap, keywords, highlights] =
-      parseOpenAIResponse(content as string);
-
-    console.log("mindmap", mindmap);
-    console.log("outlines", outlines);
+    const results = parseOpenAIResponse(content);
+    // console.log("mindmap", mindmap);
+    // console.log("outlines", outlines);
 
     return json({
       success: true,
