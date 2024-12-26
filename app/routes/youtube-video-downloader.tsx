d00e5578a2c9d47ba6be704102b8e5cfbdbde1d9
mamun2519 <juborajislam46@gmail.com>
2024-12-26 09:49:23 +0600

    feat: added to video downloader tools

--- app/routes/youtube-video-downloader.tsx
+++ app/routes/youtube-video-downloader.tsx
@@ -12,11 +12,9 @@ export const action: ActionFunction = async ({
   const url = fromData.get("url") as string;
   const format = fromData.get("format");
 
-  if (!url || !ytdl.validateURL(url)) {
-    return { error: "Invalid YouTube URL. Please enter a valid link." };
+  if (!url || !url.startsWith("https://www.youtube.com/watch")) {
+    return { error: "Invalid YouTube URL. Please provide a valid link." };
   }
-
-  const videoInfo = ytdl.getInfo(url);
   console.log("video info", videoInfo);
 
   console.log("url", url);
