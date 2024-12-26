a4f298047f34d178a6f6289420c6b099234f9c93
mamun2519 <juborajislam46@gmail.com>
2024-12-26 07:22:05 +0600

    feat: addded to youtube downloader

--- app/routes/youtube-video-downloader.tsx
+++ app/routes/youtube-video-downloader.tsx
@@ -1,4 +1,7 @@
+import { useActionData } from "@remix-run/react";
+
 const YoutubeVideoDownloader = () => {
+  const actionData = useActionData();
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
