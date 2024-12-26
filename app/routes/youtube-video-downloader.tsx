ee8269d07fc909ec80915073560a0f2da986de95
mamun2519 <juborajislam46@gmail.com>
2024-12-26 09:35:25 +0600

    feat: added to video downloader tools

--- app/routes/youtube-video-downloader.tsx
+++ app/routes/youtube-video-downloader.tsx
@@ -1,5 +1,6 @@
 import { ActionFunction } from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
+import ytdl from "ytdl-core";
 
 export const action: ActionFunction = async ({
   request,
