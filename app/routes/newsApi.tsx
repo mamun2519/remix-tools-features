ae0a828919baefcddf562a31b7bd94cf23931b9c
mamun2519 <juborajislam46@gmail.com>
2025-03-15 10:58:54 +0600

    feat: added to news api

--- app/routes/newsApi.tsx
+++ app/routes/newsApi.tsx
@@ -3,7 +3,8 @@ import axios from "axios";
 import React from "react";
 const NEWS_API_URL = "https://eventregistry.org/api/v1/article/getArticles";
 const API_KEY = "e76afccd-a989-4e02-b25e-14fde05cadd2";
-
+const TRENDING_API_URL =
+  "https://eventregistry.org/api/v1/trends/getTrendingTopics";
 function displayBreakingNews(articles: any[]) {
   console.log("Breaking News:");
   articles.forEach((article, index) => {
@@ -35,6 +36,34 @@ const fetchBreakingNews = async () => {
     return [];
   }
 };
+
+async function fetchTrendingTopics() {
+  try {
+    const response = await axios.get(TRENDING_API_URL, {
+      params: {
+        action: "getTrendingTopics",
+        lang: "eng", // English topics
+        count: 10, // Number of topics to fetch
+        apiKey: API_KEY,
+      },
+    });
+
+    // Check if the response contains data
+    if (response.data && response.data.topics) {
+      return response.data.topics;
+    } else {
+      console.error("No trending topics found in the response:", response.data);
+      return [];
+    }
+  } catch (error) {
+    console.error(
+      "Error fetching trending topics:",
+      error.response ? error.response.data : error.message,
+    );
+    return [];
+  }
+}
+
 export const loader = async () => {
   // const breakingNews = await fetchBreakingNews();
 
