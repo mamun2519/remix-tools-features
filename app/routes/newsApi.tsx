b23b240416288fd9463d0601f948c586e2eaaf3a
mamun2519 <juborajislam46@gmail.com>
2025-03-15 10:52:31 +0600

    feat: added to news api

--- app/routes/newsApi.tsx
+++ app/routes/newsApi.tsx
@@ -1,5 +1,10 @@
+import { json } from "@remix-run/node";
 import React from "react";
+const NEWS_API_URL = "https://eventregistry.org/api/v1/article/getArticles";
 
+export const loader = async () => {
+  return json({ ok: true });
+};
 const NewsApi = () => {
   return (
     <div>
