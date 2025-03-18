8a7fd8a9e95299e251db39d432c3669ae9ba05e1
mamun2519 <juborajislam46@gmail.com>
2025-03-18 12:03:16 +0600

    feat: added to youtube trending videos

--- app/routes/youtubeTrends.tsx
+++ app/routes/youtubeTrends.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const YoutubeTrends = () => {
-  return <div>Trend</div>;
+  return (
+    <div className="p-20">
+      <div className="border p-5">
+        <input name="keyword" type="text" />
+      </div>
+    </div>
+  );
 };
 
 export default YoutubeTrends;
