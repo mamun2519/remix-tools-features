0830b51f457f505095052c7e73c137d41188b53c
mamun2519 <juborajislam46@gmail.com>
2025-03-18 12:06:07 +0600

    feat: added to youtube trending videos

--- app/routes/youtubeTrends.tsx
+++ app/routes/youtubeTrends.tsx
@@ -4,6 +4,9 @@ const YoutubeTrends = () => {
   return (
     <div className="p-20">
       <div className="border p-5">
+        <h3 className="text-center text-xl font-bold uppercase">
+          YouTube Trends & Alerts{" "}
+        </h3>
         <input name="keyword" type="text" />
       </div>
     </div>
