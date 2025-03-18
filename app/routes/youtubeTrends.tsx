f4198e2f5a761c733c574f05ddf38c671cdbb636
mamun2519 <juborajislam46@gmail.com>
2025-03-18 12:12:06 +0600

    feat: added to youtube trending videos

--- app/routes/youtubeTrends.tsx
+++ app/routes/youtubeTrends.tsx
@@ -59,6 +59,11 @@ const YoutubeTrends = () => {
                 className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Minimum Views:
+              </label>
+            </div>
 
             {/* Submit Button */}
             <div>
